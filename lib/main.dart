a3a6b9b29a0e1f7566e4e6361590b17da16167f5
Nishant <nishantjoshee@gmail.com>
2020-09-13 09:58:09 +0545

    Read epub file from asset

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:epub_viewer/epub_viewer.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Simple Epub Viewer'),
+        ),
+        body: Center(
+          child: FlatButton(
+            onPressed: () {
+              EpubViewer.setConfig(
+                themeColor: Colors.deepPurple,
+                identifier: "androidBook",
+                scrollDirection: EpubScrollDirection.ALLDIRECTIONS,
+                allowSharing: true,
+                enableTts: true,
+              );
+              EpubViewer.openAsset(
+                'assets/folio.epub',
+              );
+            },
+            child: Container(
+              child: Text('open epub'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
